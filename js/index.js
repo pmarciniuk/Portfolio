737db1be4bee077a242dd66d7cbf2aa8858bb399
pmarciniuk <p.marciniuk91@gmail.com>
2017-12-17 11:01:07 +0000

    Flow changes

--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
-// MOVE TOP ICON DISPLAY
-const moveTop = document.querySelector('.move-top');
-let isScrolled = false;
-
-function onScroll() {
-    if (isScrolled) {
-        moveTop.classList.add('move-top--active');
-    } else {
-        moveTop.classList.remove('move-top--active');
-    }
-}
-
-document.addEventListener('scroll', onScroll);
-
-window.addEventListener('scroll', () => {
-    isScrolled = window.scrollY > 100 ? true : false;
-});
-
+const moveTop=document.querySelector('.move-top');let isScrolled=false;function onScroll(){if(isScrolled){moveTop.classList.add('move-top--active');}else{moveTop.classList.remove('move-top--active');}}document.addEventListener('scroll',onScroll);window.addEventListener('scroll',()=>{isScrolled=window.scrollY>100?true:false;});
